3dee8d73743cb4e29d4ccf3ab0c70a9e7ea66192
smfsenthu23 <sushimeduri@gmail.com>
2023-11-07 14:26:32 -0800

    Deploy capstone project to gh pages

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import App from "./App";
-
-test("renders app", () =>
-{
-  render(<App />);
-  const text = screen.getByText(/guided/i);
-  expect(text).toBeInTheDocument();
-});
